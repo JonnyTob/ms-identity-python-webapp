7155ba353b48f1f0562780b29c63f5122fb58c63
JonnyTobb <jonny.tobiassen@hnikt.no>
2024-10-29 21:25:05 +0100

    Minor changes

--- app.py
+++ app.py
@@ -23,17 +23,18 @@ __version__ = "0.9.0"  # The version of this sample, for troubleshooting purpose
 
 app = Flask(__name__)
 app.config.from_object(app_config)
+
 auth = Auth(
     app,
     authority=os.getenv("AUTHORITY"),
     client_id=os.getenv("CLIENT_ID"),
     client_credential=os.getenv("CLIENT_SECRET"),
     redirect_uri=os.getenv("REDIRECT_URI"),
-    oidc_authority=os.getenv("OIDC_AUTHORITY"),
-    b2c_tenant_name=os.getenv('B2C_TENANT_NAME'),
-    b2c_signup_signin_user_flow=os.getenv('SIGNUPSIGNIN_USER_FLOW'),
-    b2c_edit_profile_user_flow=os.getenv('EDITPROFILE_USER_FLOW'),
-    b2c_reset_password_user_flow=os.getenv('RESETPASSWORD_USER_FLOW'),
+    #oidc_authority=os.getenv("OIDC_AUTHORITY"),
+    #b2c_tenant_name=os.getenv('B2C_TENANT_NAME'),
+    #b2c_signup_signin_user_flow=os.getenv('SIGNUPSIGNIN_USER_FLOW'),
+    #b2c_edit_profile_user_flow=os.getenv('EDITPROFILE_USER_FLOW'),
+    #b2c_reset_password_user_flow=os.getenv('RESETPASSWORD_USER_FLOW'),
 )
 
 @app.route("/")
